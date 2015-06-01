51ad79f7c40731c41dde8f986c4d6a5d98a4f0f7
Scott Miller <scott.miller171@gmail.com>
2015-06-01 13:58:41 -0400

    messing with animated pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,7 +31,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
     // Ensure the drawer is hidden on desktop/tablet
     var drawerPanel = document.querySelector('#paperDrawerPanel');
-    drawerPanel.forceNarrow = true;
   });
 
 })(document);
